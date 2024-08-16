e74199ac2f770d2703f1c5a60f8af73018ca1c21
ZiadAS40 <zead4036@gmail.com>
2024-08-16 03:49:21 +0300

    tying to do it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""get the nubmber of subscipers"""
+"""
+get the nubmber of subscipers
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """get the number of subs"""
-    headers = {"User-Agent": "Custom"}
-    response = requests.get(("https://www.reddit.com/r/{}/about.json"
-                             .format(subreddit)),
-                            headers=headers)
-    data = response.json()
-    return data.get("data", {}).get("subscribers", 0)
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+    v1.0.0 (by /u/ZiadAS4036)'}).json()
+    num = r.get("data", {}).get("subscribers", 0)
+    return num
